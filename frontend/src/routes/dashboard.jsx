34c09e3b3ac38083d1970cdb401f31cf784c90db
Diegolf <diegolf125@gmail.com>
2019-06-04 17:59:33 -0300

    Correçao

--- frontend/src/routes/dashboard.jsx
+++ frontend/src/routes/dashboard.jsx
@@ -42,49 +42,49 @@ var dashRoutes = [
     component: QueryCertificate
   },
   {
-    
+    hidden: true,
     path: "/dashboard",
     name: "Início",
     icon: "nc-icon nc-bank",
     component: Dashboard
   },
   {
-    
+    hidden: true,
     path: "/icons",
     name: "Icons",
     icon: "nc-icon nc-diamond",
     component: Icons
   },
   {
-    
+    hidden: true,
     path: "/maps", 
     name: "Maps", 
     icon: "nc-icon nc-pin-3", 
     component: Maps
   },
   {
-    
+    hidden: true,
     path: "/notifications",
     name: "Notifications",
     icon: "nc-icon nc-bell-55",
     component: Notifications
   },
   {
-    
+    hidden: true,
     path: "/user-page",
     name: "User Profile",
     icon: "nc-icon nc-single-02",
     component: UserPage
   },
   {
-    
+    hidden: true,
     path: "/tables",
     name: "Table List",
     icon: "nc-icon nc-tile-56",
     component: TableList
   },
   {
-    
+    hidden: true,
     path: "/typography",
     name: "Typography",
     icon: "nc-icon nc-caps-small",
